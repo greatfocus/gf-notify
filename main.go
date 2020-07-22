965cca18f9f0b94f33b54dd6b1cb4b8d9d9c2f23
mucunga90 <mucunga90@gmail>
2020-07-22 16:32:28 +0300

    upgraded the frame version

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	frame "github.com/greatfocus/gf-frame"
-	"github.com/greatfocus/gf-frame/cron"
 	"github.com/greatfocus/gf-notify/router"
 	"github.com/greatfocus/gf-notify/task"
 	_ "github.com/lib/pq"
@@ -16,14 +15,14 @@ func main() {
 	// background task
 	tasks := task.Tasks{}
 	tasks.Init(server.DB, server.Config)
-	cron.Every(1).Sunday().At("8:00").Do(tasks.RunDatabaseScripts)
+	server.Cron.Every(1).Sunday().At("8:00").Do(tasks.RunDatabaseScripts)
 	// cron.Every(1).Day().At("3:00").Do(tasks.RunDatabaseScripts) // consider making API calls to users to validate gf users e.g if they have been disabled
-	cron.Every(20).Second().Do(tasks.MoveStagedToQueue)
-	cron.Every(10).Second().Do(tasks.ReQueueProcessingEmails) // in case there is panic error some queue may be stack in processing mode
-	cron.Every(10).Second().Do(tasks.SendQueuedEmails)
-	cron.Every(1).Minute().Do(tasks.MoveOutFailedQueue)
-	cron.Every(1).Minute().Do(tasks.MoveOutCompleteQueue)
-	cron.Start()
+	server.Cron.Every(20).Second().Do(tasks.MoveStagedToQueue)
+	server.Cron.Every(10).Second().Do(tasks.ReQueueProcessingEmails) // in case there is panic error some queue may be stack in processing mode
+	server.Cron.Every(10).Second().Do(tasks.SendQueuedEmails)
+	server.Cron.Every(1).Minute().Do(tasks.MoveOutFailedQueue)
+	server.Cron.Every(1).Minute().Do(tasks.MoveOutCompleteQueue)
+	server.Cron.Start()
 
 	// start API service
 	server.Start(router.Router(server.DB))
