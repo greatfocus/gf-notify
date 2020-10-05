e32282b9806c213bbf9e841c39f4f9192ef38fa6
johnmuthurimi <mucunga90@gmail.com>
2020-10-05 12:36:32 +0300

    included new frame version

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/greatfocus/gf-notify
 go 1.14
 
 require (
-	github.com/greatfocus/gf-frame v1.5.4-0.20200906185047-9c8762bf506c
+	github.com/greatfocus/gf-frame v1.0.0-beta3 
 	github.com/lib/pq v1.7.1
 )
--- router/router.go
+++ router/router.go
@@ -4,25 +4,27 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/greatfocus/gf-frame/config"
+
 	"github.com/greatfocus/gf-frame/database"
 	"github.com/greatfocus/gf-frame/middlewares"
 	"github.com/greatfocus/gf-notify/controllers"
 )
 
 // Router is exported and used in main.go
-func Router(db *database.DB) *http.ServeMux {
+func Router(db *database.DB, config *config.Config) *http.ServeMux {
 	// create new router
 	mux := http.NewServeMux()
 
 	// users
-	usersRoute(mux, db)
+	usersRoute(mux, db, config)
 
 	log.Println("Created routes with controllers")
 	return mux
 }
 
 // usersRoute created all routes and handlers relating to user controller
-func usersRoute(mux *http.ServeMux, db *database.DB) {
+func usersRoute(mux *http.ServeMux, db *database.DB, config *config.Config) {
 	// Initialize controller
 	messageController := controllers.MessageController{}
 	messageController.Init(db)
@@ -52,13 +54,13 @@ func usersRoute(mux *http.ServeMux, db *database.DB) {
 	reportController.Init(db)
 
 	// Initialize routes
-	mux.HandleFunc("/notify/channel", middlewares.SetMiddlewareJSON(channelController.Handler))
-	mux.HandleFunc("/notify/message", middlewares.SetMiddlewareJSON(messageController.Handler))
-	mux.HandleFunc("/notify/message/bulk", middlewares.SetMiddlewareJSON(messageBulkController.Handler))
-	mux.HandleFunc("/notify/dashboard", middlewares.SetMiddlewareJSON(dashboardController.Handler))
-	mux.HandleFunc("/notify/gf-user", middlewares.SetMiddlewareJSON(gfuserController.Handler))
-	mux.HandleFunc("/notify/template", middlewares.SetMiddlewareJSON(templateController.Handler))
-	mux.HandleFunc("/notify/template-message", middlewares.SetMiddlewareJSON(templateMessageController.Handler))
-	mux.HandleFunc("/notify/template-message/bulk", middlewares.SetMiddlewareJSON(templateMessageBulkController.Handler))
-	mux.HandleFunc("/notify/report", middlewares.SetMiddlewareJSON(reportController.Handler))
+	mux.HandleFunc("/notify/channel", middlewares.SetMiddlewareJSON(channelController.Handler, config.Server.AllowedOrigins))
+	mux.HandleFunc("/notify/message", middlewares.SetMiddlewareJSON(messageController.Handler, config.Server.AllowedOrigins))
+	mux.HandleFunc("/notify/message/bulk", middlewares.SetMiddlewareJSON(messageBulkController.Handler, config.Server.AllowedOrigins))
+	mux.HandleFunc("/notify/dashboard", middlewares.SetMiddlewareJSON(dashboardController.Handler, config.Server.AllowedOrigins))
+	mux.HandleFunc("/notify/gf-user", middlewares.SetMiddlewareJSON(gfuserController.Handler, config.Server.AllowedOrigins))
+	mux.HandleFunc("/notify/template", middlewares.SetMiddlewareJSON(templateController.Handler, config.Server.AllowedOrigins))
+	mux.HandleFunc("/notify/template-message", middlewares.SetMiddlewareJSON(templateMessageController.Handler, config.Server.AllowedOrigins))
+	mux.HandleFunc("/notify/template-message/bulk", middlewares.SetMiddlewareJSON(templateMessageBulkController.Handler, config.Server.AllowedOrigins))
+	mux.HandleFunc("/notify/report", middlewares.SetMiddlewareJSON(reportController.Handler, config.Server.AllowedOrigins))
 }
